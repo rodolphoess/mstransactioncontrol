115c24a2f6089a9e7351a40f56e43c942c2dbdc7
Rodolpho Sousa <rodolphoerick90@gmail.com>
2023-08-04 12:17:50 -0300

    chore: adicionando estrutura inicial do projeto

---
+++ src/main/java/br/transaction/control/TransactionControl.java
@@ -0,0 +1,13 @@
+package br.transaction.control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionControl {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionControl.class, args);
+    }
+
+}
---
+++ src/test/java/br/transaction/control/port/TempTest.java
@@ -0,0 +1,7 @@
+package br.transaction.control.port;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TempTest {
+
+}
