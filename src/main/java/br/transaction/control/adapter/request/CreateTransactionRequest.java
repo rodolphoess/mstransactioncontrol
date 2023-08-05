1106e9df7be12fd9feac461ed6d4d9c3572d908f
Rodolpho Sousa <rodolphoerick90@gmail.com>
2023-08-05 19:58:00 -0300

    feat: implementing requests and responses classes to the operations

--- src/main/java/br/transaction/control/adapter/request/CreateTransactionRequest.java
+++ src/main/java/br/transaction/control/adapter/request/CreateTransactionRequest.java
@@ -4,11 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class CreateAccountRequest {
+public class CreateTransactionRequest {
 
-    private String documentNumber;
+    private Integer accountId;
+    private Integer operationType;
+    private BigDecimal amount;
 
 }
--- src/main/java/br/transaction/control/adapter/response/AccountResponse.java
+++ src/main/java/br/transaction/control/adapter/response/AccountResponse.java
@@ -1,4 +1,15 @@
 package br.transaction.control.adapter.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AccountResponse {
+
+    private Integer accountId;
+    private String documentNumber;
+
 }
